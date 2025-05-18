0ac8b4c5c6f7af98a68f9c0c1e89aa32e31701fc
combari <combarim2@gmail.com>
2025-05-18 23:12:22 +0100

    Affichage du resultat html de lime

--- app/app.py
+++ app/app.py
@@ -1,7 +1,16 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, Response
+
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+from main import predict_email, load_model, load_vectorizer
+from lime.lime_text import LimeTextExplainer
+from sklearn.pipeline import make_pipeline
+
 
 app = Flask(__name__)
-
+model = load_model()
+vectorizer = load_vectorizer()
 
 
 @app.route("/", methods=["GET"])
@@ -16,13 +25,26 @@ def analyze():
     email_object = data.get("email_object", "")
     email_text = data.get("email_text", "")
 
-    result = {
-        "label": "Phishing",
-        "percent": 72
-    }
+    email = email_object + " " + email_text
+    email_df = pd.DataFrame([email], columns=["text"])
+    email_vec = vectorizer.transform(email_df)
 
-    return jsonify(result)
+    result = predict_email(model, email_vec)
+    prediction = "Email légitime" if result[0] == 1 else "Phishing"
+    percent = result[1][result[0]] * 100
 
+    pipeline = make_pipeline(vectorizer, model)
+    class_names = ["Email légitime", "Phishing"]
+    explainer = LimeTextExplainer(class_names=class_names)
+    exp = explainer.explain_instance(email, pipeline.predict_proba, num_features=10)
+
+    # Convert explanation to HTML string
+    html_content = exp.as_html()
+
+    print(html_content)
+
+    # Return HTML content as response
+    return Response(html_content, mimetype='text/html')
 
 
 if __name__ == "__main__":
--- app/static/script.js
+++ app/static/script.js
@@ -15,20 +15,13 @@ document.getElementById("analyze-form").addEventListener("submit", function(e) {
     body: JSON.stringify(data)
   })
   .then(res => res.json())
-  .then(data => {
-      console.log(data)
+  .then(fullHtml => {
       const container = document.getElementById('result-container');
-      container.innerHTML = `
-          <div class="progress-bar">
-              <div class="result-bar">
-                   <div class="result-bar-fill" style="width: ${data.percent}%;">${data.label} ${data.percent}%</div>
-              </div>
-          </div>
-          <div class="result-explanation">
-              Possession her thoroughly remarkably terminated man continuing. Removed greater to do ability. You shy shall while but wrote marry. Call why sake has sing pure. Gay six set polite nature worthy. So matter be me we wisdom should basket moment merely. Me burst ample wrong which would mr he could. Visit arise my point timed drawn no. Can friendly laughter goodness man him appetite carriage. Any widen see gay forth alone fruit bed.
-          </div>
-       `;
+      const parser = new DOMParser();
+      const doc = parser.parseFromString(fullHtml, 'text/html');
+      container.innerHTML = doc.body.innerHTML;
       container.scrollIntoView({ behavior: "smooth" });
+
   })
   .catch(err => {
     console.error("Erreur API :", err);
